bd85ae6f76b4d5cd6c2fa55d7a564e87841c84e5
isabellecvr <isabelleintech@gmail.com>
2020-09-12 15:09:16 -0300

    arquivos adicionados

---
+++ script.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector("h2")
+console.log(titulo)
+input = document.querySelector("input")
+console.log(input)
+var button = document.querySelector("button")
+console.log(button)
+function cliquei(){
+   event.preventDefault()
+    titulo.innerHTML=input.value
+}
